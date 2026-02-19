0f032f5621d9b3f8596117b509fd6f7a2ee9afc4
Joseph Tong <josephtong710@gmail.com>
2026-02-18 19:33:18 -0800

    done

--- Ackermann.cpp
+++ Ackermann.cpp
@@ -6,7 +6,7 @@ unsigned long long ackermann(unsigned int m, unsigned int n) {
  if (m == 0){
         return n + 1;
  } else if (m > 0 && n == 0){
-        return ackermann(m - 1, 1);
+    return ackermann(m-1, 1);
  } else if (m > 0 && n > 0){
      return ackermann(m-1, ackermann(m, n - 1));
 }
